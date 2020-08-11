2b389126b19f1dc9a131b693c115f86f44cf0986
Deepa Dangwal <dangwaldeepa19@gmail.com>
2020-08-11 16:28:48 +0530

    Updates

---
+++ precache-manifest.522f6bf7b5a9c55996914e84b299016d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825e6070751489b2a9fb49c92448fa7c",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "3240eff237d4ff874f56",
+    "url": "/me/static/css/2.ab965100.chunk.css"
+  },
+  {
+    "revision": "4a90bfeef03a95e125e0",
+    "url": "/me/static/css/main.f4a205f8.chunk.css"
+  },
+  {
+    "revision": "3240eff237d4ff874f56",
+    "url": "/me/static/js/2.c54f39e6.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/me/static/js/2.c54f39e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a90bfeef03a95e125e0",
+    "url": "/me/static/js/main.5eedb1ad.chunk.js"
+  },
+  {
+    "revision": "45f033d185bd3fc94ae0",
+    "url": "/me/static/js/runtime-main.50e3daf3.js"
+  },
+  {
+    "revision": "118b2929c257de09f0d3519f35c67b08",
+    "url": "/me/static/media/Profile.118b2929.jpg"
+  }
+]);
